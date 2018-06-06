c9cbb3daaefe77d66a37abd46ec97072ad5fd863
Jared <7060603@hotmail.ca>
2018-06-05 21:39:21 -0400

    Many updates
    - Video support
    - Stopped Playback support
    - "Running" message so you arent just staring at black
    - Starts VLC if run with the "withvlc" parameter (example: `node rpc.js withvlc`)

--- rpc.js
+++ rpc.js
@@ -1,7 +1,9 @@
 const client = require('discord-rich-presence')('410664151334256663');	//last part is the app id for discord. Dont change.
 const config=require("./config.js");
+
 var vlcService = require("droopy-vlc"),
     vlc = new vlcService("http://:"+config.vlcPass+"@"+config.vlcHost+":"+config.vlcPort);
+console.log("Running...")
 
 function escapeHtml(text) {
   return text
@@ -24,27 +26,59 @@ var nowPlaying={
 //checks for changes in playback and if it finds any it updates your presence
 function update(){
 	vlc.status().then(function(status) {
+		var secondline=""
+		if(status.isVideo){
+			secondline=escapeHtml(status.date)+" Video";
+		}else{
+			secondline=escapeHtml(status.artist+" - "+status.album);
+		}
 		var newPlaying={
-		  state: escapeHtml(status.artist+" - "+status.album),
+		  state: secondline,
 		  details: escapeHtml(status.title),
 		  largeImageKey: "vlc",
 		  smallImageKey: status.state,
 		  instance: true,
 		}
-		if(newPlaying.state!==nowPlaying.state || newPlaying.details!==nowPlaying.details || newPlaying.smallImageKey!==nowPlaying.smallImageKey){
+		if(newPlaying.state!==nowPlaying.state || newPlaying.details!==nowPlaying.details || newPlaying.smallImageKey!==nowPlaying.smallImageKey){	//if anything has changed
 			console.log("Changes detected; sending to Discord")
 			if(status.state==="playing"){
 				newPlaying.startTimestamp=Date.now()/1000
 				newPlaying.endTimestamp=parseInt(parseInt(Date.now()/1000)+(parseInt(status.duration)-parseInt(status.time)))
-				client.updatePresence(newPlaying);
-			}else{
-				client.updatePresence(newPlaying);
 			}
+			client.updatePresence(newPlaying);
 			delete newPlaying.startTimestamp
 			delete newPlaying.endTimestamp
 			nowPlaying=newPlaying
 		}
-	});
+	}, function (error){	//if nothing is playing
+		var newPlaying={
+			state: "Stopped",
+			details: "What did you expect this to say?",
+			largeImageKey: "vlc",
+			smallImageKey: "stopped",
+			instance: true,
+		}
+		client.updatePresence(newPlaying);
+	}
+	);
+}
+
+if(process.argv.includes("withvlc")){	//if you want vlc to open with
+	const { spawn } = require('child_process');
+	var command=""
+	if(process.platform==="win32"){
+		command=config.windowsvlc
+	}else if(process.platform==="linux"){
+		command=config.linuxvlc
+	}else if(process.platform==="darwin"){
+		command=config.macvlc
+	}else{
+		command="vlc"
+	}
+	var child = spawn(command, [])	//launch vlc
+	child.on("exit", () => {
+		process.exit(0)
+	})
 }
 
 update()
