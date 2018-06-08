c65eb8f215a2db4211ac3eac1b6ca65e8cfc5f04
Jared <7060603@hotmail.ca>
2018-06-07 22:13:03 -0400

    good

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ exports.vlcHost="localhost"	//shouldnt need to change but whatever
 //Start commands
 exports.windowsvlc=`C:/Program Files/VideoLAN/VLC/vlc.exe`
 exports.linuxvlc=`vlc`
-exports.macvlc=`/Applications/VLC.app/Contents/MacOS/VLC`   //idk anything about macs so help me out here
+exports.macvlc=`/Applications/VLC.app/Contents/MacOS/VLC`
