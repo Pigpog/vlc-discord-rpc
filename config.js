031bfa2866fe8819ac80e1c29dda7b2f3fc0ce57
Jared <7060603@hotmail.ca>
2019-05-10 00:33:32 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-exports.vlcPass="rosebud"	//Lua HTTP password
-exports.vlcPort=8080		//default value
-exports.vlcHost="localhost"	//shouldnt need to change but whatever
-//Start commands
-exports.windowsvlc=`C:/Program Files/VideoLAN/VLC/vlc.exe`
-exports.linuxvlc=`vlc`
-exports.macvlc=`/Applications/VLC.app/Contents/MacOS/VLC`
