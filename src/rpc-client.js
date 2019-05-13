491874292f777c06ae971cbe39847ab706f7213c
Jared <7060603@hotmail.ca>
2019-05-13 15:16:44 -0400

    what

--- src/rpc-client.js
+++ src/rpc-client.js
@@ -6,19 +6,49 @@ const rich = require('discord-rich-presence');
 const buildPresence=require("./helpers/buildPresence.js");
 const vlcjs = require('vlc.js');
 const VLCClient = new vlcjs.Client(vlc);
+var timeInactive=0;
+var loggedIn=false;
 
-const meta = {
-    "lastFileName": ""
+var meta = {
+    "lastFileName": "",
+    "lastState":""
+}
+
+function sleep(client){
+    console.log("Sleeping");
+    client.disconnect();
+    loggedIn=false;
+}
+
+function wake(client){
+    console.log("Waking");
+    client=rich(discord.appId);
+    loggedIn=true;
 }
 
 function update(client) {
     VLCClient.getStatus()
     .then(status => {
-        if (status.information.category.meta.filename != meta.lastFileName) {
+        if(status.state==="paused" || status.state==="stopped"){
+            if(loggedIn){
+                timeInactive+=discord.updateInterval;
+                if(timeInactive>discord.sleepTime){
+                    sleep(client);
+                }
+            }
+        }else{
+            timeInactive=0;
+            if(!loggedIn){
+                wake(client);
+            }
+        }
+        if((status.information.category.meta.filename !== meta.lastFileName || status.state !== meta.lastState) && loggedIn) {
+            
             console.log("Changes detected.");
             client.updatePresence(buildPresence(status));
         }
         meta.lastFileName = status.information.category.meta.filename;
+        meta.lastState = status.state;
     })
     .catch(console.log);
 }
@@ -26,6 +56,7 @@ function update(client) {
 module.exports = id => {
     const RPCClient = rich(id)
     .on('connected', () => {
+        loggedIn=true;
         console.log("Connected");
         update(RPCClient);
         setInterval(update, discord.updateInterval, RPCClient);	//check for updates repeatedly
