800074074716896ea77a62b9e9fe0d8f8c73ebf9
dylhack <dylhack@gmail.com>
2019-05-14 15:41:40 -0500

    Updated prettier

--- src/presence/format.js
+++ src/presence/format.js
@@ -5,29 +5,30 @@
  * @param {Object} status
  */
 module.exports = status => {
-	var secondLine;
-	if (
-		status.information.category.meta.filename.match(
-			/^.*\.(avi|AVI|wmv|WMV|flv|FLV|mpg|MPG|mp4|MP4|mkv|MKV)$/
-		)
-	) {
-		secondLine = (status.date || "") + " Video";
-	} else if (status.information.category.meta.now_playing) {
-		secondLine = status.information.category.meta.now_playing;
-	} else {
-		secondLine =
-			(status.information.category.meta.artist ||
-				status.information.category.meta.ALBUMARTIST) +
-			" - " +
-			status.information.category.meta.album;
-	}
-	return {
-		state: secondLine,
-		details:
-			status.information.category.meta.title ||
-			status.information.category.meta.filename,
-		largeImageKey: "vlc",
-		smallImageKey: status.state,
-		instance: true
-	};
+  var secondLine;
+  if (
+    status.information.category.meta.filename.match(
+      /^.*\.(avi|AVI|wmv|WMV|flv|FLV|mpg|MPG|mp4|MP4|mkv|MKV)$/
+    )
+  ) {
+    secondLine = (status.date || "") + " Video";
+  } else if (status.information.category.meta.now_playing) {
+    secondLine = status.information.category.meta.now_playing;
+  } else {
+    secondLine =
+      (status.information.category.meta.artist ||
+        status.information.category.meta.ALBUMARTIST) +
+      " - " +
+      status.information.category.meta.album;
+  }
+  return {
+    state: secondLine,
+    details:
+      status.information.category.meta.title ||
+      status.information.category.meta.filename,
+    largeImageKey: "vlc",
+    smallImageKey: status.state,
+    instance: true
+  };
 };
+
