3249d40781a554fffd28f916c04b80f82114541d
dylhack <dylhack@gmail.com>
2019-05-15 18:57:48 -0500

    Removed persisting TypeError

--- src/presence/update.js
+++ src/presence/update.js
@@ -8,41 +8,66 @@ const config = require("../../config/config.json");
 const VLCClient = new VLC.Client(config.vlc);
 const buildPresence = require("./format.js");
 var meta = {
-  lastFileName: "",
-  lastState: "",
-  asleep: false,
-  timeInactive: 0
+	lastFileName: "",
+	lastState: "",
+	asleep: false,
+	timeInactive: 0
 };
 
 module.exports = client => {
-  VLCClient.getStatus()
-    .then(status => {
-      if (status.state === "paused" || status.state === "stopped") {
-        if (!meta.asleep) {
-          meta.timeInactive += config.rpc.updateInterval;
-          if (meta.timeInactive > config.rpc.sleepTime) {
-            console.log("Sleeping");
-            meta.asleep = true;
-          }
-        }
-      } else {
-        meta.timeInactive = 0;
-        if (meta.asleep) {
-          console.log("Waking");
-          meta.asleep = false;
-        }
-      }
-      if (
-        (status.information.category.meta.filename !== meta.lastFileName ||
-          status.state !== meta.lastState) &&
-        !meta.asleep
-      ) {
-        console.log("Changes detected.");
-        client.updatePresence(buildPresence(status));
-      }
-      meta.lastFileName = status.information.category.meta.filename;
-      meta.lastState = status.state;
-    })
-    .catch(console.log);
+	VLCClient.getStatus()
+		.then(status => {
+			if (
+				status.state === "paused" ||
+				status.state === "stopped"
+			) {
+				if (!meta.asleep) {
+					meta.timeInactive +=
+						config.rpc.updateInterval;
+					if (
+						meta.timeInactive >
+						config.rpc.sleepTime
+					) {
+						console.log("Sleeping");
+						meta.asleep = true;
+					}
+				}
+			} else {
+				meta.timeInactive = 0;
+				if (meta.asleep) {
+					console.log("Waking");
+					meta.asleep = false;
+				}
+			}
+			if (status.state != "stopped") {
+				if (
+					(status.information.category.meta
+						.filename !==
+						meta.lastFileName ||
+						status.state !==
+							meta.lastState) &&
+					!meta.asleep
+				) {
+					console.log("Changes detected.");
+					client.updatePresence(
+						buildPresence(status)
+					);
+				}
+			} else {
+				if (meta.lastState != status.state) {
+					client.updatePresence({
+						state: "Stopped",
+						details: "Nothing is playing.",
+						largeImageKey: "vlc",
+						smallImageKey: "stopped",
+						instance: true
+					});
+				}
+			}
+			meta.lastFileName = status.information
+				? status.information.category.meta.filename
+				: undefined;
+			meta.lastState = status.state;
+		})
+		.catch(console.log);
 };
-
