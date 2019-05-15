7a495b8b8a4b56e917d11c0b226312884b5eaa80
dylhack <dylhack@gmail.com>
2019-05-15 18:27:04 -0500

    Feature: Time!

--- src/presence/format.js
+++ src/presence/format.js
@@ -5,30 +5,31 @@
  * @param {Object} status
  */
 module.exports = status => {
-  var secondLine;
-  if (
-    status.information.category.meta.filename.match(
-      /^.*\.(avi|AVI|wmv|WMV|flv|FLV|mpg|MPG|mp4|MP4|mkv|MKV)$/
-    )
-  ) {
-    secondLine = (status.date || "") + " Video";
-  } else if (status.information.category.meta.now_playing) {
-    secondLine = status.information.category.meta.now_playing;
-  } else {
-    secondLine =
-      (status.information.category.meta.artist ||
-        status.information.category.meta.ALBUMARTIST) +
-      " - " +
-      status.information.category.meta.album;
-  }
-  return {
-    state: secondLine,
-    details:
-      status.information.category.meta.title ||
-      status.information.category.meta.filename,
-    largeImageKey: "vlc",
-    smallImageKey: status.state,
-    instance: true
-  };
+	var secondLine;
+	var meta = status.information.category.meta;
+	if (
+		meta.filename.match(
+			/^.*\.(avi|AVI|wmv|WMV|flv|FLV|mpg|MPG|mp4|MP4|mkv|MKV)$/
+		)
+	) {
+		secondLine = (status.date || "") + " Video";
+	} else if (meta.now_playing) {
+		secondLine = meta.now_playing;
+	} else {
+		if (meta.artist) {
+			secondLine += meta.artist;
+			if (meta.album) secondLine += " - " + meta.album;
+		}
+	}
+	return {
+		state: secondLine,
+		details: meta.title || meta.filename,
+		largeImageKey: "vlc",
+		smallImageKey: status.state,
+		instance: true,
+		endTimestamp: status.state == "playing" ? Math.floor(
+			(Date.now() / 1000 + (status.length - status.time)) /
+				status.rate 
+		) : undefined 
+	};
 };
-
