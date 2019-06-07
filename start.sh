1b96122b0028e0b1a1c476683e6d2eb746e00923
Pigpog <7060603@hotmail.ca>
2019-06-06 20:57:20 -0400

    Added shell script

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+pause() {
+    read -p "Press any key to exit . . ."
+    exit
+}
+
+if ! [ -d "node_modules" ]; then
+    npm i
+    if [ $? -eq 127 ]; then
+        echo ---------------------------------------------
+        echo "           Please install npm."
+        echo ---------------------------------------------
+        pause
+    elif ! [ $? -eq 0 ]; then
+        echo ---------------------------------------------
+        echo " An error occurred while installing modules."
+        echo ---------------------------------------------
+        pause
+    fi
+fi
+
+npm start
+
+if ! [ $? -eq 0 ]; then
+    pause
+fi
