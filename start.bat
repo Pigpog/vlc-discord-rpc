002357b627eaa9a49832cc0bcbdf51ce555888bd
Pigpog <7060603@hotmail.ca>
2019-06-09 00:40:24 -0400

    Improve windows support
    Add error messages that are visible when run invisibly. Removed exit statement from npm start catch statement

--- start.bat
+++ start.bat
@@ -3,16 +3,13 @@ if NOT EXIST node_modules (
     ECHO Did not detect node_modules directory; Installing modules...
     CALL npm i --silent
     if NOT EXIST node_modules (
-        ECHO ---------------------------------------------------------------------------
-        ECHO A problem occurrred while installing modules. Ensure that npm is installed.
-        ECHO ---------------------------------------------------------------------------
-        PAUSE
+        START CMD /C "ECHO A problem occurrred while installing modules. Ensure that npm is installed. && PAUSE"
         EXIT
     )
     ECHO Modules installed.
 )
 ECHO Starting script...
-CALL  npm start --silent
+CALL npm start
 IF %ERRORLEVEL% EQU 1 (
-    PAUSE
+    START CMD /C "ECHO A problem occurrred while launching vlc. && PAUSE"
 )
