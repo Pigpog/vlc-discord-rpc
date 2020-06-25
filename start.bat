c053e7404ec8f849f58aa1914ef515a43d9647ba
Pigpog <7060603@hotmail.ca>
2020-06-24 22:20:44 -0400

    Fix typo

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ if NOT EXIST node_modules (
     ECHO Did not detect node_modules directory; Installing modules...
     CALL npm i --production --silent
     if NOT EXIST node_modules (
-        START CMD /C "ECHO A problem occurrred while installing modules. Ensure that npm is installed. && PAUSE"
+        START CMD /C "ECHO A problem occurred while installing modules. Ensure that npm is installed. && PAUSE"
         EXIT
     )
     ECHO Modules installed.
@@ -11,5 +11,5 @@ if NOT EXIST node_modules (
 ECHO Starting script...
 CALL npm start
 IF %ERRORLEVEL% EQU 1 (
-    START CMD /C "ECHO A problem occurrred while launching vlc. && PAUSE"
+    START CMD /C "ECHO A problem occurred while launching vlc. && PAUSE"
 )
