3d705a8e567a2362312241725679180ad2796c05
Pigpog <7060603@hotmail.ca>
2021-08-27 23:22:46 -0400

    Make debugging easier on Windows

--- start.bat
+++ start.bat
@@ -1,9 +1,11 @@
 @echo off
+
 if NOT EXIST node_modules (
     ECHO Did not detect node_modules directory; Installing modules...
     CALL npm i --production --silent
     if NOT EXIST node_modules (
-        START CMD /C "ECHO A problem occurred while installing modules. Ensure that npm is installed. && PAUSE"
+        ECHO A problem occurred while installing modules. Ensure that npm is installed.
+	PAUSE
         EXIT
     )
     ECHO Modules installed.
